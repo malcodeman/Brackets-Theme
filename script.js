9ca11c05fb0090c5437eb14f61f811a01dd16549
malcodeman <malcodeman@users.noreply.github.com>
2017-06-24 15:10:08 +0200

    Functional menu

---
+++ script.js
@@ -0,0 +1,22 @@
+function openMenu() {
+    "use strict";
+    var nav = document.getElementById("nav"), nav_content = document.getElementById("nav_content"), menu = document.getElementById("menu");
+    if (window.getComputedStyle(nav_content).display === "none") {
+        nav.classList.add("nav-full");
+        nav_content.classList.add("flex");
+        menu.classList.remove("ic-menu");
+        menu.classList.add("ic-close");
+        document.body.classList.add("hide-overflow");
+    } else {
+        nav.classList.remove("nav-full");
+        nav_content.classList.remove("flex");
+        menu.classList.remove("ic-close");
+        menu.classList.add("ic-menu");
+        document.body.classList.remove("hide-overflow");
+    }
+}
+function main() {
+    "use strict";
+    document.getElementById("menu").addEventListener("click", openMenu);
+}
+main();
