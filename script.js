e9f1a49d516cbd673f947dc4f5ea45631cfef042
malcodeman <malcodeman@users.noreply.github.com>
2017-06-25 13:27:04 +0200

    Fixed menu overflow

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function openMenu() {
     "use strict";
-    var nav = document.getElementById("nav"), nav_content = document.getElementById("nav_content"), menu = document.getElementById("menu");
+    var nav = document.getElementById("nav"), nav_content = document.getElementById("nav_content"), menu = document.getElementById("menu"), header_wrapper = document.getElementById("header_wrapper"), main_content = document.getElementById("main_content");
     if (window.getComputedStyle(nav_content).display === "none") {
         nav.classList.add("nav-full");
-        nav_content.classList.add("flex");
+        nav_content.classList.add("flex", "pt-3", "pb-3");
         menu.classList.remove("ic-menu");
         menu.classList.add("ic-close");
-        document.body.classList.add("hide-overflow");
+        header_wrapper.classList.add("static");
+        main_content.classList.add("none");
     } else {
         nav.classList.remove("nav-full");
-        nav_content.classList.remove("flex");
+        nav_content.classList.remove("flex", "pt-3", "pb-3");
         menu.classList.remove("ic-close");
         menu.classList.add("ic-menu");
-        document.body.classList.remove("hide-overflow");        
+        header_wrapper.classList.remove("static");
+        main_content.classList.remove("none");
     }
 }
 function main() {
