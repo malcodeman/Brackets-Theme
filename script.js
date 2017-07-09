eb0feb89e9853e04c03c6f5d12ac0eb3f2f4385a
malcodeman <malcodeman@users.noreply.github.com>
2017-07-09 14:10:45 +0200

    Added social nav

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ function openMenu() {
     var nav = document.getElementById("nav"), nav_content = document.getElementById("nav_content"), menu = document.getElementById("menu"), header_wrapper = document.getElementById("header_wrapper"), main_content = document.getElementById("main_content");
     if (window.getComputedStyle(nav_content).display === "none") {
         nav.classList.add("nav-full");
-        nav_content.classList.add("flex", "pt-3", "pb-3");
+        nav_content.classList.add("flex");
         menu.classList.remove("ic-menu");
         menu.classList.add("ic-close");
         header_wrapper.classList.add("static");
         main_content.classList.add("none");
     } else {
         nav.classList.remove("nav-full");
-        nav_content.classList.remove("flex", "pt-3", "pb-3");
+        nav_content.classList.remove("flex");
         menu.classList.remove("ic-close");
         menu.classList.add("ic-menu");
         header_wrapper.classList.remove("static");
