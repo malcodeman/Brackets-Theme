e6c374d4fc4276f6dba4624e458a39554ebb7945
malcodeman <malcodeman@users.noreply.github.com>
2017-07-09 16:49:59 +0200

    Javascript is now readable

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 function openMenu() {
     "use strict";
-    var nav = document.getElementById("nav"), nav_content = document.getElementById("nav_content"), menu = document.getElementById("menu"), header_wrapper = document.getElementById("header_wrapper"), main_content = document.getElementById("main_content"), nav_social = document.getElementById("nav_social");
+    var nav, nav_content, menu, header_wrapper, main_content, nav_social;
+    nav = document.getElementById("nav");
+    nav_content = document.getElementById("nav_content");
+    menu = document.getElementById("menu");
+    header_wrapper = document.getElementById("header_wrapper");
+    main_content = document.getElementById("main_content");
+    nav_social = document.getElementById("nav_social");
     if (window.getComputedStyle(nav_content).display === "none") {
         nav.classList.add("nav-full");
         nav_content.classList.add("flex");
+        nav_social.classList.add("flex");
         menu.classList.remove("ic-menu");
         menu.classList.add("ic-close");
         main_content.classList.add("none");
-        nav_social.classList.add("flex");
     } else {
         nav.classList.remove("nav-full");
         nav_content.classList.remove("flex");
+        nav_social.classList.remove("flex");
         menu.classList.remove("ic-close");
         menu.classList.add("ic-menu");
         main_content.classList.remove("none");
-        nav_social.classList.remove("flex");
     }
 }
 function main() {
