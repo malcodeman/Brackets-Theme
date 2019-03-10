a0874ec1b43a4395ff96f89d20cec4786ef461d2
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2019-03-10 16:31:59 +0100

    Adds prettier

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 # Code Editor Theme
+
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/malcodeman/code-editor-theme/blob/master/LICENSE)
+
 Template for code editor or similar software.
 
 ![Homepage image](screenshots/homepage.png)
 
 ## Assets
 
-### Backgrounds
-* [Brickwall](https://www.toptal.com/designers/subtlepatterns/brick-wall/)
-
-### Fonts
-* [Roboto](https://fonts.google.com/specimen/Roboto)
-
-### Icons
-* [Menu](https://material.io/icons/#ic_menu)
-* [Close](https://material.io/icons/#ic_close)
-* [Github](https://feathericons.com/node_modules/feather-icons/dist/icons/github.svg)
-* [Twitter](https://feathericons.com/node_modules/feather-icons/dist/icons/twitter.svg)
+- [Brickwall background](https://www.toptal.com/designers/subtlepatterns/brick-wall/)
+- [Roboto font](https://fonts.google.com/specimen/Roboto)
+- [Menu icon](https://material.io/icons/#ic_menu)
+- [Close icon](https://material.io/icons/#ic_close)
+- [Github icon](https://feathericons.com/node_modules/feather-icons/dist/icons/github.svg)
+- [Twitter icon](https://feathericons.com/node_modules/feather-icons/dist/icons/twitter.svg)
 
 ## License
+
 This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,33 @@
 function openMenu() {
-    "use strict";
-    var header_wrapper, nav, nav_max, nav_social, ic_menu;
-    header_wrapper = document.getElementById("header_wrapper");
-    nav = document.getElementById("nav");
-    nav_max = document.getElementById("nav_max");
-    nav_social = document.getElementById("nav_social");
-    ic_menu = document.getElementById("ic_menu");
-    if (window.getComputedStyle(nav_max).display === "none") {
-        document.body.classList.add("hidden");
-        header_wrapper.classList.add("header-wrapper-full");
-        nav.classList.add("nav-full");
-        nav_max.classList.add("flex");
-        nav_social.classList.add("flex");
-        ic_menu.classList.remove("ic-menu");
-        ic_menu.classList.add("ic-close");
-    } else {
-        document.body.classList.remove("hidden");
-        header_wrapper.classList.remove("header-wrapper-full");
-        nav.classList.remove("nav-full");
-        nav_max.classList.remove("flex");
-        nav_social.classList.remove("flex");
-        ic_menu.classList.remove("ic-close");
-        ic_menu.classList.add("ic-menu");
-    }
+  "use strict";
+  var header_wrapper, nav, nav_max, nav_social, ic_menu;
+  header_wrapper = document.getElementById("header_wrapper");
+  nav = document.getElementById("nav");
+  nav_max = document.getElementById("nav_max");
+  nav_social = document.getElementById("nav_social");
+  ic_menu = document.getElementById("ic_menu");
+  if (window.getComputedStyle(nav_max).display === "none") {
+    document.body.classList.add("hidden");
+    header_wrapper.classList.add("header-wrapper-full");
+    nav.classList.add("nav-full");
+    nav_max.classList.add("flex");
+    nav_social.classList.add("flex");
+    ic_menu.classList.remove("ic-menu");
+    ic_menu.classList.add("ic-close");
+  } else {
+    document.body.classList.remove("hidden");
+    header_wrapper.classList.remove("header-wrapper-full");
+    nav.classList.remove("nav-full");
+    nav_max.classList.remove("flex");
+    nav_social.classList.remove("flex");
+    ic_menu.classList.remove("ic-close");
+    ic_menu.classList.add("ic-menu");
+  }
 }
+
 function main() {
-    "use strict";
-    document.getElementById("ic_menu").addEventListener("click", openMenu);
+  "use strict";
+  document.getElementById("ic_menu").addEventListener("click", openMenu);
 }
+
 main();
