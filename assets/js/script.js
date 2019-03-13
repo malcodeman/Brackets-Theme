9f94c88f1b2bc9cc9eda2ac56c58e9589d2440ee
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2019-03-13 18:03:14 +0100

    Adds general improvements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,21 @@
-function openMenu() {
-  "use strict";
-  var header_wrapper, nav, nav_max, nav_social, ic_menu;
-  header_wrapper = document.getElementById("header_wrapper");
-  nav = document.getElementById("nav");
-  nav_max = document.getElementById("nav_max");
-  nav_social = document.getElementById("nav_social");
-  ic_menu = document.getElementById("ic_menu");
-  if (window.getComputedStyle(nav_max).display === "none") {
-    document.body.classList.add("hidden");
-    header_wrapper.classList.add("header-wrapper-full");
-    nav.classList.add("nav-full");
-    nav_max.classList.add("flex");
-    nav_social.classList.add("flex");
-    ic_menu.classList.remove("ic-menu");
-    ic_menu.classList.add("ic-close");
-  } else {
-    document.body.classList.remove("hidden");
-    header_wrapper.classList.remove("header-wrapper-full");
-    nav.classList.remove("nav-full");
-    nav_max.classList.remove("flex");
-    nav_social.classList.remove("flex");
-    ic_menu.classList.remove("ic-close");
-    ic_menu.classList.add("ic-menu");
-  }
+function toggleMenu() {
+  const headerWrapper = document.getElementById("headerWrapper");
+  const nav = document.getElementById("nav");
+  const icMenu = document.getElementById("icMenu");
+  const navMax = document.getElementById("navMax");
+  const navSocial = document.getElementById("navSocial");
+  const content = document.getElementById("content");
+
+  headerWrapper.classList.toggle("header-wrapper-full");
+  nav.classList.toggle("nav-full");
+  icMenu.classList.toggle("ic-close");
+  navMax.classList.toggle("nav-max-visible");
+  navSocial.classList.toggle("nav-social-visible");
+  content.classList.toggle("content-hidden");
 }
 
 function main() {
-  "use strict";
-  document.getElementById("ic_menu").addEventListener("click", openMenu);
+  document.getElementById("icMenu").addEventListener("click", toggleMenu);
 }
 
 main();
